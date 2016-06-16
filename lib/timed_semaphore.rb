421374484e2d47fa1b464d23469cf2c8b5581896
ssuljic <sadzid.etf@gmail.com>
2016-06-16 11:31:04 +0200

    Improved documentation

--- lib/timed_semaphore.rb
+++ lib/timed_semaphore.rb
@@ -1,4 +1,30 @@
+# TimedSemaphore is a specialized implementation of a Semaphore
+# that gives a number of permits in a given time frame. A use case
+# for it is to limit the load on a resource.
+# The idea is taken from the Apache Commons Lang package.
+# @see https://commons.apache.org/proper/commons-lang/javadocs/api-3.1/org/apache/commons/lang3/concurrent/TimedSemaphore.html
+#   Apache Commons Lang
+#
+# @author ssuljic
+#
+# @example
+#   threads = []
+#   semaphore = TimedSemaphore.new(2, 3)
+#
+#   10.times do
+#     threads << Thread.new do
+#       semaphore.acquire
+#       puts Time.now
+#     end
+#   end
+#
+#   threads.map(&:join)
 class TimedSemaphore
+  # @param num_of_ops [Fixnum] Number of operations
+  #   which should be allowed in a specified time frame.
+  # @param num_of_seconds [Fixnum] Period in seconds after all
+  #   permits are released.
+  # @return [TimedSemaphore] a new instance of TimedSemaphore.
   def initialize(num_of_ops, num_of_seconds)
     @count = 0
     @limit = num_of_ops
@@ -8,7 +34,10 @@ class TimedSemaphore
     @timer = nil
   end
 
-  # Acquires a permit or sleeps the thread if all permits are exhausted
+  # Tries to acquire a permit from the semaphore. This method
+  # will block if the limit for the current period has already
+  # been reached. The first call starts a timer thread for releasing
+  # all permits, which makes the semaphore active
   def acquire
     synchronize do
       @condition.wait while @limit > 0 && @count == @limit
@@ -19,7 +48,8 @@ class TimedSemaphore
 
   private
 
-  # Starts a thread which releases all permits after @period seconds
+  # Starts a timer thread which releases
+  # all permits after @period seconds.
   def start_timer
     synchronize do
       @timer = Thread.new do
@@ -29,7 +59,8 @@ class TimedSemaphore
     end
   end
 
-  # Releases all permits and notifies all waiting threads to try acquire again
+  # Releases all permits and notifies all
+  # waiting threads to try acquire again.
   def release_permits
     synchronize do
       @timer = nil
@@ -38,6 +69,8 @@ class TimedSemaphore
     end
   end
 
+  # Method used for synchronizing on @lock.
+  # Syntactic sugar
   def synchronize(&block)
     fail 'No block given' unless block_given?
     @lock.synchronize(&block)
